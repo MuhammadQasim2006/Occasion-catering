8189f2271e187b6c3afb65dc8df82b5b4b0932d0
Muhammad Qasim Moos <qasimmoos2006@gmail.com>
2026-09-04 15:19:15 +0200

    Include categories and menu items in packages API

--- occasion-backend/src/controllers/packageController.js
+++ occasion-backend/src/controllers/packageController.js
@@ -1,9 +1,30 @@
-const { CateringPackage } = require("../models");
+const {
+  CateringPackage,
+  Category,
+  MenuItem,
+} = require("../models");
 
 async function getPackages(req, res) {
   try {
     const packages = await CateringPackage.findAll({
       order: [["package_id", "ASC"]],
+      include: [
+        {
+          model: Category,
+          attributes: ["category_id", "name"],
+        },
+        {
+          model: MenuItem,
+          attributes: [
+            "menu_item_id",
+            "name",
+            "description",
+            "price_addon",
+            "is_default",
+          ],
+          order: [["menu_item_id", "ASC"]],
+        },
+      ],
     });
 
     res.status(200).json(packages);
