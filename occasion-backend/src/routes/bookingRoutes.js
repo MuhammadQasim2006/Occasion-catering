49197d0aa20c06c61d44569da53b7a9b31d9d925
Muhammad Qasim Moos <qasimmoos2006@gmail.com>
2026-09-07 11:24:10 +0200

    Protect booking routes with JWT authentication

--- occasion-backend/src/routes/bookingRoutes.js
+++ occasion-backend/src/routes/bookingRoutes.js
@@ -31,4 +31,3 @@ router.get(
 );
 
 module.exports = router;
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0LCJyb2xlIjoiY3VzdG9tZXIiLCJpYXQiOjE3ODg2OTc1MjgsImV4cCI6MTc4ODcwNDcyOH0.eyUAQFILaWRYJsTP1jcEJ-XH99-FP67_pztqYrhhIQM
